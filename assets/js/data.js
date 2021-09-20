4c611a5cf89a7ec3de2f7cee046740b4852abdba
LastUnityCreative <7alicalik98@gmail.com>
2021-09-20 15:40:56 +0300

    calculator

---
+++ assets/js/data.js
@@ -0,0 +1,16 @@
+const Users = [{
+        id: 1,
+        Adı: Ali,
+        Soyadı: Çalık
+    },
+    {
+        id: 2,
+        Adı: Murat,
+        Soyadı: Gencer
+    },
+    {
+        id: 3,
+        Adı: Gözde,
+        Soyadı: Serap
+    },
+]
